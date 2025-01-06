11aac57003511812c209d14820f2442acc9f3de6
group10adp <group10adp@gmail.com>
2025-01-06 17:32:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import uvicorn
 from fastapi import FastAPI, HTTPException
+import firebase_admin
 from firebase_admin import credentials, firestore, initialize_app
 import numpy as np
 from sklearn.linear_model import LinearRegression
